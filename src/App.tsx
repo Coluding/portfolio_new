3e86bbd0a74e767347d01b54f3bc70f45f552055
Lukas Bierling <98786106+Coluding@users.noreply.github.com>
2024-02-29 11:20:27 +0100

    skilsl

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {ThemeProvider, CssBaseline } from "@mui/material";
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga4';
-
-
 
 import HomePage from './container/HomePage';
 import BlogView from './components/blog/BlogView';
@@ -12,22 +7,8 @@ import MathBlog from './components/blog/BlogContent';
 import ProjectView from './components/projects/ProjectView';
 import theme from './utils/theme';
 
-
-const usePageTracking = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
-  }, [location]);
-};
-
-
 function App() {
-  useEffect(() => {
-    ReactGA.initialize('G-TKJW5X955X');
-  }, []);
 
-  usePageTracking();
 
   return (
     <ThemeProvider theme={theme}>
