53e2381dc86a6694662c5253c46d09e64ccd6b94
Coluding <bierling.lukas@gmail.com>
2024-04-04 02:07:30 +0200

    pdf

--- src/components/blog/BlogContent.tsx
+++ src/components/blog/BlogContent.tsx
@@ -1,4 +1,5 @@
-import { Box, Divider, Typography, useTheme, Fade, useMediaQuery } from "@mui/material";
+import React from 'react';
+import { Box, Divider, Typography, useTheme, Fade, useMediaQuery, styled } from "@mui/material";
 import { useParams } from 'react-router-dom';
 import 'katex/dist/katex.min.css';
 import { convertToCustomFormat } from '../../utils/latex';
@@ -12,14 +13,33 @@ const MathBlog = () => {
   const blog = blogs.find(blog => blog.id === parseInt(id || ""));
   const timestamp = new Date(blog?.date || "").toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" });
 
+  const IframeContainer = styled('div')(({ theme }) => ({
+    overflow: 'hidden',
+    paddingBottom: '56.25%', // Aspect ratio for 16:9, adjust as needed
+    position: 'relative',
+    height: 0,
+    width: '100%',
+  }));
+
+  const Iframe = styled('iframe')({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    border: 'none', // Remove iframe border
+  });
+
   return (
-    <Box margin={"0%"}>
+    <Box margin="0%">
       <Fade in={true} timeout={1500}>
         <Box paddingLeft={"15%"} paddingRight={"15%"} paddingTop={"2%"} paddingBottom={"10%"}>
           {blog && (
             <>
-              <Typography variant={"h2"} sx={{ textAlign: "center", padding: theme.spacing(2) }}>{blog.title}</Typography>
-              <Divider sx={{ width: '100%', fontWeight: "bold" }}></Divider>
+              <Typography variant="h2" sx={{ textAlign: "center", padding: theme.spacing(2) }}>
+                {blog.title}
+              </Typography>
+              <Divider sx={{ width: '100%', fontWeight: "bold" }} />
               <Box sx={{
                 position: 'absolute',
                 top: isMobile ? 1 : theme.spacing(2),
@@ -30,13 +50,9 @@ const MathBlog = () => {
               {blog.content.map((content, index) => (
                 <Box key={index} sx={{ padding: theme.spacing(2) }} marginTop={"2%"}>
                   {content.endsWith('.pdf') ? (
-                    <iframe 
-                    src={new URL( "/assets/" + content, import.meta.url).href} 
-                      width="100%" 
-                      height="1000px" // Adjust height as needed
-                      frameBorder="0"
-                      allowFullScreen
-                    ></iframe>
+                    <IframeContainer>
+                      <Iframe src={new URL( "/assets/" + content, import.meta.url).href}  allowFullScreen />
+                    </IframeContainer>
                   ) : (
                     <LatexRenderer text={convertToCustomFormat(content)} />
                   )}
