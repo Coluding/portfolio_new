8f18f028c46522770af1c81177b21a0bf7437422
Lukas  Bierling <lukasbierling@Laptop-von-Lukas.local>
2024-12-07 22:44:04 +0100

    new cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {ThemeProvider, CssBaseline } from "@mui/material";
+import AnimatedCursor from "react-animated-cursor"
+
 
 import HomePage from './container/HomePage';
 import BlogView from './components/blog/BlogView';
@@ -13,6 +15,27 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
         <CssBaseline/>
+        <AnimatedCursor
+      innerSize={15}
+      outerSize={15}
+      color='0, 0, 0'
+      outerAlpha={0.2}
+      innerScale={1.2}
+      outerScale={2}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]}
+    />
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
